c366b104fe37905c8a07a4c0385c88bf864b4a0c
drewpl2021 <linoplas0@gmail.com>
2025-04-25 12:25:46 -0500

    cambios

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import AOS from 'aos';  // Importar la librería AOS
 import 'aos/dist/aos.css';
-import {ActivatedRoute} from '@angular/router';
-import {SectionsDetailService} from '../../services/sectionsDetail.service';  // Importar el archivo CSS de AOS
+import {ActivatedRoute, RouterModule} from '@angular/router';
+import {SectionsDetailService} from '../../services/sectionsDetail.service';
+import {CommonModule} from '@angular/common';  // Importar el archivo CSS de AOS
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [
+    CommonModule, // Asegúrate de incluir CommonModule aquí
+    RouterModule
+  ],
 })
 export class HomeComponent implements OnInit {
   sectionId: string = '';  // Variable para almacenar el ID de la sección
   sectionDetails: any = {};
   constructor(private route: ActivatedRoute,
-              private sectionsDetailService: SectionsDetailService ) {}
+              private sectionsDetailService: SectionsDetailService,
+              ) {}
 
   ngOnInit(): void {
-    // Obtener el id de la URL
     this.route.paramMap.subscribe(params => {
       this.sectionId = params.get('id') || '';  // Asignar el 'id' a la variable
       console.log(`Section ID from URL: ${this.sectionId}`);
@@ -26,8 +31,17 @@ export class HomeComponent implements OnInit {
         // Llamar al método del servicio que obtiene los detalles
         this.sectionsDetailService.getsectionDetailsById(this.sectionId).subscribe(
           (data) => {
-            this.sectionDetails = data; // Asignamos los datos a la variable sectionDetails
-            console.log('Section details:', this.sectionDetails);  // Logueamos los detalles
+            // Ordenar los detalles por el campo 'code'
+            const orderedData = Object.keys(data)
+              .map(key => data[key])  // Convertir el objeto a un array de valores
+              .sort((a, b) => {
+                // Convertir los 'code' a números y ordenarlos
+                return Number(a.code) - Number(b.code);
+              });
+
+            // Asignamos los datos ordenados a la variable sectionDetails
+            this.sectionDetails = orderedData;
+            console.log('Ordered Section details:', this.sectionDetails);  // Logueamos los detalles ordenados
           },
           (error) => {
             console.error('Error fetching section details:', error);  // Manejo de errores
@@ -36,4 +50,7 @@ export class HomeComponent implements OnInit {
       }
     });
   }
+
+
+
 }
