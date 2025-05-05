fa1fb9ef4b4c888c42963422da28f42bb51240dc
Luchito <103071836+LuisAlbertoQ@users.noreply.github.com>
2025-05-04 22:05:15 -0600

    cambios

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import AOS from 'aos';  // Importar la librería AOS
+import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+  imageList = [
+    'assets/images/inicio.png',
+    'assets/images/inicio2.png',
+    'assets/images/inicio3.png'
+  ];
+  currentImage = this.imageList[0];
+  currentIndex = 0;
 
-  ngOnInit() {
-    AOS.init({
-      once: false,
-    });
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.imageList.length;
+      this.currentImage = this.imageList[this.currentIndex];
+    }, 4000); // cambia cada 4 segundos
   }
 
   scrollLeft(): void {
