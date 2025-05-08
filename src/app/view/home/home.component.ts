e0bb0629475b91896ba08b7b56bc2d7b27dbfbfa
Luchito <103071836+LuisAlbertoQ@users.noreply.github.com>
2025-05-08 12:14:33 -0600

    final

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {NgStyle} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    NgStyle
+    NgStyle,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
--- src/app/view/places/places.component.ts
+++ src/app/view/places/places.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-places',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './places.component.html',
   styleUrl: './places.component.css'
 })
