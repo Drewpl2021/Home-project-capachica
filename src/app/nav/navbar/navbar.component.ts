e00abec5a06386fc65d60a8a112eca3fb69bd066
drewpl2021 <linoplas0@gmail.com>
2025-05-15 00:00:58 -0500

    cambios

--- src/app/nav/navbar/navbar.component.ts
+++ src/app/nav/navbar/navbar.component.ts
@@ -1,29 +1,46 @@
 import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-import {Router, NavigationEnd, RouterModule, ActivatedRoute } from '@angular/router'; // Importa NavigationEnd
+import {Router, NavigationEnd, RouterModule, ActivatedRoute} from '@angular/router'; // Importa NavigationEnd
 import { filter } from 'rxjs/operators';
 import {CommonModule, NgClass} from '@angular/common';
-import {CarritoService} from '../carrito-sidebar/carrito.service';
-import {CarritoSidebarComponent} from '../carrito-sidebar/carrito-sidebar.component'; // Importa el operador filter
-
-
+import {CarritoService} from '../carrito-popup/carrito.service';
+import {CarritoSidebarComponent} from '../carrito-popup/carrito-sidebar.component';
+import {MunicipalidadService} from '../../services/municipalidad.service';
+import {SectionsService} from '../../services/sections.service'; // Importa el operador filter
+interface NavItem {
+  label: string;
+  path: string;
+  children?: NavItem[]; // Opcional, para submenús
+}
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule, CarritoSidebarComponent],
   templateUrl: './navbar.component.html',
+
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
   isScrolled = false;
-  navItems: { label: string; path: string }[] = [];
+  navItems: NavItem[] = [];
   mostrarPopup = false;
-
+  municipalidades: any[] = [];
+  distrito: string = ''; // Valor inicial vacío
+  sections: any[] = [];
   currentUrl: string = '';
   currentFragment: string | null = null;
+  dropdownOpenIndex: number | null = null;
 
   @ViewChild(CarritoSidebarComponent) carritoSidebar!: CarritoSidebarComponent;
 
-  constructor(private router: Router, private route: ActivatedRoute, public carritoService: CarritoService) {
+
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              public carritoService: CarritoService,
+              private municipalidadService: MunicipalidadService,
+              private sectionsService: SectionsService
+            ) {
+    // Escucha los eventos de NavigationEnd para resetear el scroll
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe(() => {
@@ -44,7 +61,13 @@ export class NavbarComponent implements OnInit {
     }
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50; // Ajusta este valor según necesites
+  }
   ngOnInit(): void {
+    this.loadMunicipalidades();
+    this.loadSections();
     this.setupScrollListener();
     this.setupNavItems();
     // Escuchar cambios de ruta y fragmento para activar el nav
@@ -56,48 +79,70 @@ export class NavbarComponent implements OnInit {
         this.currentFragment = fragment;
       });
     });
-  }
 
-  setupNavItems(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        const url = event.urlAfterRedirects;
-        if (url.includes('/llachon') ||
-          url.includes('/chifron') ||
-          url.includes('/ccotos') ||
-          url.includes('/isla-tikonata') ||
-          url.includes('/siale-paramis') ||
-          url.includes('/escallani')) {
-          this.navItems = [
-            { label: 'Inicio', path: '/' },
-            { label: 'Llachon', path: '/llachon' },
-            { label: 'Chifron', path: '/chifron' },
-            { label: 'Ccotos', path: '/ccotos' },
-            { label: 'Isla-Tikonata', path: '/isla-tikonata' },
-            { label: 'Siale-Paramis', path: '/siale-paramis' },
-            { label: 'Escallani', path: '/escallani' },
-            { label: 'Iniciar Sesión', path: '/sign-in' }
-          ];
-        } else {
-          this.navItems = [
-            { label: 'Inicio', path: '/' },
-            { label: 'Acerca de', path: '/about' },
-            { label: 'Lugares', path: '/places' },
-            { label: 'Hoteles', path: '/hotel' },
-            { label: 'Blog', path: '/blog' },
-            { label: 'Contacto', path: '/contact' },
-            { label: 'Iniciar Sesión', path: '/sign-in' }
-          ];
-        }
+    this.distrito = "Capachica";
+    this.municipalidadService.getMunicipalidades().subscribe((data: any) => {
+      if (data && data.content && data.content[0]) {
+        this.distrito = data.content[0].distrito;
       }
     });
   }
 
+  setupNavItems(): void {
+    const lugares: NavItem[] = [
+      { label: 'Llachon', path: '/llachon' },
+      { label: 'Chifron', path: '/chifron' },
+      { label: 'Ccotos', path: '/ccotos' },
+      { label: 'Isla-Tikonata', path: '/isla-tikonata' },
+      { label: 'Siale-Paramis', path: '/siale-paramis' },
+      { label: 'Escallani', path: '/escallani' }
+    ];
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.isScrolled = window.scrollY > 50; // Ajusta este valor según necesites
+    this.navItems = [
+      { label: 'Inicio', path: '/' },
+      { label: 'Acerca de', path: '/about' },
+      { label: 'Lugares', path: '/places', children: lugares },
+      { label: 'Hoteles', path: '/hotel' },
+      { label: 'Blog', path: '/blog' },
+      { label: 'Contacto', path: '/contact' },
+      { label: 'Iniciar Sesión', path: '/sign-in' }
+    ];
   }
+
+  toggleDropdown(index: number): void {
+    if (this.dropdownOpenIndex === index) {
+      this.dropdownOpenIndex = null;
+    } else {
+      this.dropdownOpenIndex = index;
+    }
+  }
+
+  closeDropdown(): void {
+    this.dropdownOpenIndex = null;
+  }
+
+
+
+  loadMunicipalidades(): void {
+    this.municipalidadService.getMunicipalidades().subscribe(
+      (data) => {
+        this.municipalidades = data;
+      },
+    );
+  }
+  loadSections(): void {
+    this.sectionsService.getSections().subscribe(
+      (data) => {
+        this.sections = data.content && Array.isArray(data.content) ? data.content : [];
+        console.log("secciones encontradas", this.sections)
+      },
+      (error) => {
+        console.error('Error fetching sections:', error);
+        this.sections = [];
+      }
+    );
+  }
+
 
   navigateTo(path: string): void {
     this.router.navigate([path]);
